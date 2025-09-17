ca023a8737ef2d56c03868c354aebfbddfb4feed
noname7777G <57804930+noname7777G@users.noreply.github.com>
2025-09-17 17:11:20 -0400

    Added Rulling class

---
+++ Ruling.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ScryfallObjects;
+public class Ruling : BaseObject {
+	public Ruling(string objectType, string oracleId, string source, string publishedAt, string comment) : base(objectType) {
+		this.OracleId = oracleId;
+		this.Source = source;
+		this.PublishedAt = publishedAt;
+		this.Comment = comment;
+	}
+
+	[JsonPropertyName("oracle_id")]
+	public string OracleId { get; set; }
+	[JsonPropertyName("source")]
+	public string Source { get; set; }
+	[JsonPropertyName("published_at")]
+	public string PublishedAt { get; set; }
+	[JsonPropertyName("comment")]
+	public string Comment { get; set; }
+
+	public static List<Ruling> LoadRullings(string bulkDataPath) {
+		string jsonIn = File.ReadAllText(bulkDataPath);
+		List<Ruling> rulings = JsonSerializer.Deserialize<List<Ruling>>(jsonIn) ?? throw new Exception("Could not load rulings data");
+		return rulings;
+	}
+}
